06e7cc2a5af8548d2b782c9158bf1a78d7d397af
dan2443 <68905500+dan2443@users.noreply.github.com>
2021-03-26 17:28:44 +0300

    Add background and rank

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import { particlesOptions } from './particles.js';
 import './App.css';
+
+
 
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles' params={particlesOptions}/>
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/*<FaceRecogintion />*/}
     </div>
---
+++ src/particles.js
@@ -0,0 +1,25 @@
+export const particlesOptions ={
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  },
+  interactivity: {
+    detect_on: 'window',
+    events:{
+      onhover: {
+      enable: true,
+      mode: 'repulse'
+      }
+    },
+    modes:{
+      "repulse": {
+        "distance": 80,
+      }
+    } 
+  }
+}
